e52a7cf5f31d35f5dc039a3be392ed315b8c1679
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-06-03 01:08:16 +0500

    masala ishlandi

---
+++ js/index.js
@@ -0,0 +1,117 @@
+// integer9
+// let yuz = +prompt("uch honali son kiriting");
+// let y = Math.trunc(yuz/100);
+// console.log(y);
+
+
+// integer10
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10);
+// let on = o%10;
+// console.log(b, on);
+
+// // integer11
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// // let y = Math.trunc(yuz/100);
+// let rezu = b+o+y;
+// console.log(rezu);
+
+// // integer12
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// let y = Math.trunc(yuz/100);
+// let rezu = b*100;
+// let s = o*10;
+// let d = rezu+s+y;
+// console.log(d)
+
+// integer13
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// let y = Math.trunc(yuz/100);
+// let rezu = b*10;
+// let s = o*100;
+// let d = rezu+s+y;
+// console.log(d);  
+
+// integer14
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// let y = Math.trunc(yuz/100);
+// let rezu = b*100;
+// let s = y*10;
+// let d = rezu+s+o;
+// console.log(d) 
+
+// integer15
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// let y = Math.trunc(yuz/100);
+// let rezu = y*10;
+// let s = o*100;
+// let d = rezu+s+b;
+// console.log(d);  
+
+// integer16
+// let yuz = +prompt("uch honali son kiriting");
+// let b = yuz%10;
+// let o = Math.trunc(yuz/10)%10;
+// let y = Math.trunc(yuz/100);
+// let rezu = y*100;
+// let s = b*10;
+// let d = rezu+s+o;
+// console.log(d);  
+
+// integer17
+// let ming = +prompt("999 dan katta son kiriting");
+// let yuz = Math.trunc(ming/100);
+// let rezu = yuz%10;
+// console.log(rezu);
+
+// integer18
+// let ming = +prompt("999 dan katta son kiriting");
+// let yuz = Math.trunc(ming/1000);
+// let rezu = yuz%10;
+// console.log(rezu);
+
+// integer19
+// let n = +prompt("kun boshidan beri qancha sekunt o`tganini kiriting");
+// let m = Math.trunc(n/60);
+// console.log(m);
+
+// integer20
+// let n = +prompt("kun boshidan beri qancha sekunt o`tganini kiriting");
+// let m = Math.trunc(n/60);
+// let s = Math.trunc(m/60);
+// console.log(s);
+
+// integer21
+// let n = +prompt("kun boshidan beri qancha sekunt o`tganini kiriting");
+// let m = Math.trunc(n/60);
+// let s = n%60;
+// console.log(m);
+// console.log(s);
+
+// integer22
+// let n = +prompt("kun boshidan beri qancha sekunt o`tganini kiriting");
+// let c = n%3600;
+// let s = Math.trunc(n/3600);
+// console.log(s);
+// console.log(c);
+
+// integer23
+// let n = +prompt("kun boshidan beri qancha sekunt o`tganini kiriting");
+// let c = n%3600;
+// let m = Math.trunc(c/60);
+// let sekunt = c%60;
+// let s = Math.trunc(n/3600);
+// console.log(sekunt);
+// console.log(m);
+// console.log(s);
